e0c0c00ee4f4ec891d1be9f51695b732f1054d35
Ihor <45120893+ihorkozar@users.noreply.github.com>
2019-06-29 17:18:06 +0300

    v2

--- app/src/main/java/i/kozar/coursera_course2_testapp3/MainActivity.java
+++ app/src/main/java/i/kozar/coursera_course2_testapp3/MainActivity.java
@@ -6,79 +6,105 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonStart;
-    ProgressBar progressBar;
-    TextView textView;
-
-    SleepService sleepService;
-    boolean mBound = false;
-
-    private BroadcastReceiver mLocalBroadcast = new BroadcastReceiver(){
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            /** Пытался какой-то костыль навесить
-             * boolean finish = intent.getBooleanExtra("finnish", false);
-            if (finish){
-                unbindService(mConnection);
-                mBound = false;
-                buttonStart.setEnabled(true);
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
-                textView.setText(R.string.text_for_finish);
-            }
-             **/
-
-        }
-    };
-
+    private Button buttonStart;
+    private SleepTask mSleepTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar = findViewById(R.id.progressBar);
-        textView = findViewById(R.id.text);
+
+        mSleepTask = new SleepTask(MainActivity.this);
+
         buttonStart = findViewById(R.id.btn_start);
-
-        LocalBroadcastManager.getInstance(this).registerReceiver(mLocalBroadcast ,
-                new IntentFilter("myBroadcast"));
-
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonStart.setEnabled(false);
-                progressBar.setVisibility(ProgressBar.VISIBLE);
-                textView.setText(R.string.text_for_TextView);
-                Intent intent = new Intent(MainActivity.this, SleepService.class);
-                bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-                //start task
+                mSleepTask.execute();
             }
         });
     }
 
-    private ServiceConnection mConnection = new ServiceConnection() {
+    TextView getTextView() {
+        return findViewById(R.id.text);
+    }
 
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            SleepService.LocalBinder binder = (SleepService.LocalBinder) service;
-            sleepService = binder.getService();
-            mBound = true;
+    ProgressBar getProgressBar() {
+        return findViewById(R.id.progressBar);
+    }
+
+    Button getButton(){
+        return findViewById(R.id.btn_start);
+    }
+
+    /**
+     * Асинк таск :
+     */
+
+    private static class SleepTask extends AsyncTask<String, Void, Bitmap> {
+        private WeakReference<MainActivity> mActivityWeakReference;
+
+        private SleepTask(MainActivity activity) {
+            mActivityWeakReference = new WeakReference<>(activity);
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
+        protected void onPreExecute() {
+            MainActivity activity = mActivityWeakReference.get();
+            if (activity != null) {
+                activity.getProgressBar().setVisibility(View.VISIBLE);
+                activity.getTextView().setText(R.string.text_for_TextView);
+                activity.getButton().setEnabled(false);
+            }
         }
-    };
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            MainActivity activity = mActivityWeakReference.get();
+            if (activity != null) {
+                activity.getProgressBar().setVisibility(View.INVISIBLE);
+                activity.getTextView().setText(R.string.text_for_finish);
+                activity.getButton().setEnabled(true);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            MainActivity mainActivity = mActivityWeakReference.get();
+            if (mainActivity != null) {
+                mainActivity.getProgressBar().setVisibility(View.INVISIBLE);
+                Toast.makeText(mainActivity, "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
